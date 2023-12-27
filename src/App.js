b062a55acceb8fa06c57b584ec6f95e7f424e28e
Valentyn <wolf.valentyn@gmail.com>
2023-12-27 18:05:03 +0200

    integrate React Router DOM into the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <Register />
+    <Router>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
