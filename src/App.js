94dd3b8e1ed39790daa1510ebd9be37fd8b4a00a
Valentyn <wolf.valentyn@gmail.com>
2023-12-27 19:08:27 +0200

    add ProtectedRoute with conditional redirection to login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
 const App = () => {
+  const currentUser = false;
+
+  const ProtectedRoute = ({children}) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  }
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+      <Route path="/">
+          <Route index element={
+            <ProtectedRoute>
+              <MainPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
       </Routes>
     </Router>
   );
