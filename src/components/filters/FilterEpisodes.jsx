dc21915a587f3ea118d68ba2a2782d9e0a0041f0
Valentyn <wolf.valentyn@gmail.com>
2024-01-08 18:01:00 +0200

    add FilterEpisodes component layout

---
+++ src/components/filters/FilterEpisodes.jsx
@@ -0,0 +1,16 @@
+const FilterEpisodes = () => {
+  return (
+    <div className="anime__filters-section">
+      <div className="anime__filters-title">Episodes</div>
+      <div className="anime__filters-wrapper">
+        <div className="anime__filters-item">1 - 11</div>
+        <div className="anime__filters-item">12</div>
+        <div className="anime__filters-item">24</div>
+        <div className="anime__filters-item">24 - 100</div>
+        <div className="anime__filters-item">100+</div>
+      </div>
+    </div>
+  )
+}
+
+export default FilterEpisodes;
