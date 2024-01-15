24cf858bd4137b1fffd9a8c2d8d97ce506e3a624
Valentyn <wolf.valentyn@gmail.com>
2024-01-15 19:59:27 +0200

    add Link to Home page in header logo

--- src/components/appHeader/AppHeader.jsx
+++ src/components/appHeader/AppHeader.jsx
@@ -7,7 +7,7 @@ import './appHeader.scss';
 const AppHeader = () => {
   return (
     <header className="app__header">
-      <div className="app__header-logo">Anime List</div>
+      <Link to='/' className="app__header-logo">Anime List</Link>
       <AnimeSearchForm />
       <button className="button">
         <Link to='/login'>Sign in</Link>
