45af3f255894cc7db14b1a296d89799afe27cf93
Valentyn <wolf.valentyn@gmail.com>
2024-02-16 20:27:51 +0200

    replace static data with API in TopAnimeSlider

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -5,8 +5,11 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({baseUrl: 'https://api.jikan.moe/v4'}),
   tagTypes: ['Anime'],
   endpoints: builder => ({
-
+    getTopAnime: builder.query({
+      query: () => '/seasons/now'
+    })
   })
 })
 
+export const {useGetTopAnimeQuery} = apiSlice;
 export default apiSlice.reducer;
--- src/components/topAnimeSlider/TopAnimeSlider.jsx
+++ src/components/topAnimeSlider/TopAnimeSlider.jsx
@@ -1,11 +1,50 @@
+import { useMemo } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
+import { useGetTopAnimeQuery } from '../../api/apiSlice';
 
 import 'swiper/css';
 import "swiper/css/autoplay";
 import './topAnimeSlider.scss';
 
 const TopAnimeSlider = () => {
+  const {
+    data: anime = [],
+    isLoading, 
+    isError
+  } = useGetTopAnimeQuery();
+
+  const data = anime.data;
+  const topAnime = useMemo(() => {
+    if (!anime || !anime.data || !anime.data.length) {
+      return [];
+    }
+
+    const topAnime = data.slice();
+    return topAnime.slice(0, 10);
+  }, [data])
+
+  const items = topAnime.map((anime) => {
+    const {mal_id, images, title_english, synopsis, genres} = anime;
+    const img = images.jpg.large_image_url;
+    const descr = synopsis.length > 510 ? synopsis.slice(0, 510) + '...' : synopsis;
+    const genresString = genres.map(item => item.name).join(', ');
+
+    return (
+      <SwiperSlide className="top-anime-slider__slide" key={mal_id}>
+        <img src={img} alt="animeImg" className="top-anime-slider__img" />
+        <div className="top-anime-slider__info">
+          <div className="title_fz30fw600 top-anime-slider__title">{title_english}</div>
+          <div className="top-anime-slider__descr">{descr}</div>
+          <div className="top-anime-slider__genre">
+            <i className='icon-tag'></i>
+            <div className="top-anime-slider__genre-text">{genresString}</div>
+          </div>
+        </div>
+      </SwiperSlide>
+    )
+    })
+
   return (
     <Swiper
       modules={[Autoplay]}
@@ -19,19 +58,7 @@ const TopAnimeSlider = () => {
       }}
       className='top-anime-slider'
     >
-      {[...Array(10)].map((_, index) => (
-        <SwiperSlide className="top-anime-slider__slide" key={index}>
-          <img src="https://cdn.myanimelist.net/images/anime/1028/117777.jpg" alt="animeImg" className="top-anime-slider__img" />
-          <div className="top-anime-slider__info">
-            <div className="title_fz30fw600 top-anime-slider__title">Mushoku Tensei: Isekai Ittara Honki Dasu Part 2</div>
-            <div className="top-anime-slider__descr">Second half of Mushoku Tensei: Isekai Ittara Honki Dasu.</div>
-            <div className="top-anime-slider__genre">
-              <i className='icon-tag'></i>
-              <div className="top-anime-slider__genre-text">Drama, Fantasy, Ecchi</div>
-            </div>
-          </div>
-        </SwiperSlide>
-      ))}
+      {items}
     </Swiper>
   )
 }
