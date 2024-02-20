ac2a2992a65133996f3ff6eecb10d157b4afb371
Valentyn <wolf.valentyn@gmail.com>
2024-02-20 18:54:03 +0200

    make AnimeCard dynamic by receiving props

--- src/components/animeCard/AnimeCard.jsx
+++ src/components/animeCard/AnimeCard.jsx
@@ -1,11 +1,11 @@
 import './animeCard.scss';
 
-const AnimeCard = () => {
+const AnimeCard = ({img, episodes, title}) => {
   return (
     <div className='anime__card'>
-      <img src='https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/f154230aab3191aba977f337d392f812.jpe' alt="anime-card" className="anime__card-img" />
-      <div className="anime__card-episodes">1088</div>
-      <div className="title_fz14fw500 anime__card-title">ONE PIECE</div>
+      <img src={img} alt="anime-card" className="anime__card-img" />
+      <div className="anime__card-episodes">{episodes}</div>
+      <div className="title_fz14fw500 anime__card-title">{title}</div>
     </div>
   )
 }
