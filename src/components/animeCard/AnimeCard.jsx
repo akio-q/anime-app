6a55a110847b50fa5b67dfdfdf1d9db5b2df833e
Valentyn <wolf.valentyn@gmail.com>
2024-02-22 16:43:58 +0200

    fix bug with displaying 0 episodes in AnimeCard

--- src/components/animeCard/AnimeCard.jsx
+++ src/components/animeCard/AnimeCard.jsx
@@ -1,10 +1,12 @@
 import './animeCard.scss';
 
 const AnimeCard = ({img, episodes, title}) => {
+  const displayEpisodes = episodes ? episodes : 0; 
+
   return (
     <div className='anime__card'>
       <img src={img} alt="anime-card" className="anime__card-img" />
-      <div className="anime__card-episodes">{episodes}</div>
+      <div className="anime__card-episodes">{displayEpisodes}</div>
       <div className="title_fz14fw500 anime__card-title">{title}</div>
     </div>
   )
