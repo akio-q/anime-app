7b76a071c5dd4d8f4b642ebd613189690facc63a
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-04-22 19:19:04 +0300

    upgrade AnimeList component to dynamically render fetched anime data

--- src/components/animeList/AnimeList.jsx
+++ src/components/animeList/AnimeList.jsx
@@ -1,25 +1,29 @@
+import { useSelector } from "react-redux";
 import AnimeCard from "../animeCard/AnimeCard"
+import Spinner from "../Spinner/Spinner";
+import { ErrorMessage } from "formik";
 
 const AnimeList = () => {
+  const { animeData, animeLoadingStatus } = useSelector(state => state.anime);
+
+  if (animeLoadingStatus === 'loading') {
+    return <Spinner />
+  } else if (animeLoadingStatus === 'error') {
+    return <ErrorMessage />
+  }
+
+  const renderAnimeList = (arr) => {
+    return arr.map(item => (
+      <AnimeCard key={item.mal_id} id={item.mal_id} data={item} />
+    ))
+  }
+
+  const items = renderAnimeList(animeData);
   return (
     <>
       <div className="title_fz25fw500 anime__list-title">Search Results:</div>
       <div className="anime__list">
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
-        <AnimeCard />
+        {items}
       </div>
     </>
   )
