d04c3093862d4d470c6e239704b9590b1f97100a
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-05-18 20:29:33 +0300

    several fixes in AppFooter

--- src/components/appFooter/AppFooter.jsx
+++ src/components/appFooter/AppFooter.jsx
@@ -3,8 +3,8 @@ import './appFooter.scss';
 const AppFooter = () => {
   const date = new Date().getFullYear()>2024&&document.write("-"+new Date().getFullYear());
   return (
-    <footer class="app__footer">
-        <a href="#" class="app__footer-link">2024{date} Anime List, © All rights reserved </a>
+    <footer className="app__footer">
+        <a href="#" className="app__footer-link">2024{date} Anime List, © All rights reserved </a>
     </footer>
   )
 }
