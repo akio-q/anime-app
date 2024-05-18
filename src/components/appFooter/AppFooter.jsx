ed5e25a2b782624fa872516308e070972f114f7d
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-05-18 20:18:33 +0300

    add link with date to AppFooter

--- src/components/appFooter/AppFooter.jsx
+++ src/components/appFooter/AppFooter.jsx
@@ -1,9 +1,10 @@
 import './appFooter.scss';
 
 const AppFooter = () => {
+  const date = new Date().getFullYear()>2024&&document.write("-"+new Date().getFullYear());
   return (
-    <footer className="app__footer">
-      
+    <footer class="app__footer">
+        <a href="#" class="app__footer-link">2024{date} Anime List, © All rights reserved </a>
     </footer>
   )
 }
