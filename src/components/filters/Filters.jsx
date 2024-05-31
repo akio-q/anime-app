7f38051581a87408c3295cdcf089659ac734f6c3
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-05-31 18:50:24 +0300

    fetch genre and year filter options from api

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,9 +1,38 @@
-import { useState } from 'react';
+import { useGetAnimeGenresQuery, useGetAnimeSeasonsQuery } from "../../api/apiSlice";
 import ReactMultiSelectCheckboxes from "react-multiselect-checkboxes";
+
+import Spinner from "../Spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
 import './filters.scss';
 
 const Filters = () => {
+  const { 
+    data: animeGenres = {}, 
+    loading: isGenresLoading, 
+    error: isGenresError 
+  } = useGetAnimeGenresQuery();
+  const {
+    data: animeSeasons = {},
+    loading: isSeasonsLoading,
+    error: isSeasonsError
+  } = useGetAnimeSeasonsQuery();
+
+  if (isGenresLoading || isSeasonsLoading) {
+    return <Spinner />
+  } else if (isGenresError || isSeasonsError) {
+    return <ErrorMessage errorStatus={429} />
+  }
+
+  const genresOptions = animeGenres.data ? animeGenres.data.map(item => {
+    const nameLowerCase = item.name.toLowerCase(); 
+    return { value: nameLowerCase, label: item.name }
+  }) : [];
+  const yearOptions = animeSeasons.data ? animeSeasons.data.map(({year}) => {
+    const yearString = year.toString();
+    return { value: yearString, label: yearString }
+  }) : [];
+
   return (
     <div className="anime__filters">
       <div className="anime__filters-title title title_fz25fw500">Filter Anime</div>
@@ -28,27 +57,17 @@ const Filters = () => {
             <div className="title_fz18fw600">Year:</div>
             <ReactMultiSelectCheckboxes 
               className="react-select-container"
-              options={[
-                { value: '2024', label: '2024' },
-                { value: '2023', label: '2023' },
-                { value: '2022', label: '2022' },
-                { value: '2021', label: '2021' },
-              ]}
+              options={yearOptions}
               hideSearch={true}
               rightAligned={true}
-              placeholderButtonLabel="Select Genre"
+              placeholderButtonLabel="Select Year"
             />
           </div>
           <div className="anime__filters-item">
             <div className="title_fz18fw600">Genre:</div>
             <ReactMultiSelectCheckboxes 
               className="react-select-container"
-              options={[
-                { value: 'isekai', label: 'Isekai' },
-                { value: 'horror', label: 'Horror' },
-                { value: 'fantasy', label: 'Fantasy' },
-                { value: 'school', label: 'School' },
-              ]}
+              options={genresOptions}
               hideSearch={true}
               rightAligned={true}
               placeholderButtonLabel="Select Genre"
@@ -63,6 +82,12 @@ const Filters = () => {
                 { value: '2', label: '2' },
                 { value: '3', label: '3' },
                 { value: '4', label: '4' },
+                { value: '5', label: '5' },
+                { value: '6', label: '6' },
+                { value: '7', label: '7' },
+                { value: '8', label: '8' },
+                { value: '9', label: '9' },
+                { value: '10', label: '10' },
               ]}
               hideSearch={true}
               rightAligned={true}
@@ -88,12 +113,11 @@ const Filters = () => {
             <ReactMultiSelectCheckboxes 
               className="react-select-container"
               options={[
-                { value: 1, label: '1' },
-                { value: 2, label: '2' },
-                { value: 3, label: '3' },
-                { value: 4, label: '4' },
-                { value: 5, label: '5' },
-                
+                { value: '1', label: '1' },
+                { value: '12+', label: '12+' },
+                { value: '24+', label: '24+' },
+                { value: '100+', label: '100+' },  
+                { value: '?', label: '?' }
               ]}
               hideSearch={true}
               rightAligned={true}
