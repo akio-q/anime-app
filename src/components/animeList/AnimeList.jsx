2b79e0514ed3f89f40737d513b3c89b640e145fa
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-06-06 16:12:05 +0300

    implement filtering of AnimeList based on user-selected filters

--- src/components/animeList/AnimeList.jsx
+++ src/components/animeList/AnimeList.jsx
@@ -1,11 +1,54 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import AnimeCard from "../animeCard/AnimeCard"
 import Spinner from "../Spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import namiSticker from '../../resources/img/nami_sticker.png';
 
+const checkEpisodesMatch = (episodes, episodesSet) => {
+  if (episodesSet.size === 0) return true;
+  if (episodesSet.has(episodes.toString())) return true;
+
+  const episodeNumber = parseInt(episodes);
+
+  if (episodesSet.has('12+') && episodeNumber >= 12 & episodeNumber < 24) return true;
+  if (episodesSet.has('24+') && episodeNumber >= 24 & episodeNumber < 100) return true;
+  if (episodesSet.has('100+') && episodeNumber >= 100) return true;
+
+  return false;
+};
+
+const filterData = (data, filters) => {
+  const { season, year, genre, rating, status, episodes } = filters;
+
+  const seasonSet = new Set(season.map(filter => filter.value));
+  const yearSet = new Set(year.map(filter => filter.value));
+  const genreSet = new Set(genre.map(filter => filter.value));
+  const ratingSet = new Set(rating.map(filter => filter.value));
+  const statusSet = new Set(status.map(filter => filter.value));
+  const episodesSet = new Set(episodes.map(filter => filter.value));
+
+  return data.filter(item => {
+    const matchesSeason = !item.sesason || seasonSet.size === 0 || seasonSet.has(item.season.toLowerCase());
+    const matchesYear = !item.year || yearSet.size === 0 || yearSet.has(item.year.toString());
+    const matchesGenre = !item.genres || genreSet.size === 0 || item.genres.some(genre => genreSet.has(genre.name.toLowerCase()));
+    const matchesRating = !item.score || ratingSet.size === 0 || ratingSet.has(Math.round(item.score).toString());
+    const matchesStatus = !item.status || statusSet.size === 0 || statusSet.has(item.status.toLowerCase());
+    const matchesEpisodes = !item.episodes || episodesSet.size === 0 || checkEpisodesMatch(item.episodes, episodesSet);
+
+    return matchesSeason && matchesYear && matchesGenre && matchesRating && matchesStatus && matchesEpisodes;
+  });
+}
+
 const AnimeList = () => {
   const { filters, loadingStatus } = useSelector(state => state.filters);
+  const [filteredAnimeList, setFilteredAnimeList] = useState([]);
+
+  useEffect(() => {
+    const filteredList = filterData(filters.search, filters);
+    setFilteredAnimeList(filteredList);
+    console.log(filteredAnimeList);
+  }, [filters]);
 
   if (loadingStatus === 'loading') {
     return <Spinner />
@@ -33,7 +76,7 @@ const AnimeList = () => {
     }
   }
 
-  const animeList = renderAnimeList(filters.search);
+  const animeList = renderAnimeList(filteredAnimeList);
   return (
     <>
       {animeList}
