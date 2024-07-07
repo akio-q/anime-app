ec542db9a0d3153961fe472d689cb0acaf3502ef
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-07-07 20:29:22 +0300

    dynamically render RecentAnimeRecommendation s from API data

--- src/components/recentAnimeRecommendations/RecentAnimeRecommendations.jsx
+++ src/components/recentAnimeRecommendations/RecentAnimeRecommendations.jsx
@@ -1,23 +1,49 @@
+import { useMemo } from 'react';
+import { useGetRecentAnimeRecommendationsQuery } from '../../api/apiSlice';
+
+import Spinner from '../Spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+
 import './recentAnimeRecommendations.scss';
 
 const RecentAnimeRecommendations = () => {
+  const {
+    data: recentRecommendations = {},
+    isLoading, 
+    isError,
+    error
+  } = useGetRecentAnimeRecommendationsQuery();
+
+  const data = useMemo(() => {
+    if (!recentRecommendations || !recentRecommendations.data || !recentRecommendations.data.length) {
+      return [];
+    }
+
+    const data = recentRecommendations.data.slice();
+    return data.slice(0, 10);
+  }, [recentRecommendations])
+
+  if (isLoading) {
+    return <Spinner />
+  } else if (isError) {
+    return <ErrorMessage errorStatus={error.status} />
+  }
+
+  const renderRecentAnimeRecommendations = (arr) => {
+    return arr.map(item => (
+      <div className="recent-recommendations__card" key={item.mal_id}>
+        <img src={item.entry[0].images.webp.large_image_url} alt="" className="recent-recommendations__card-img" />
+        <div className="title_fz16fw500">{item.entry[0].title}</div>
+      </div>
+    ))
+  }
+
+  const items = renderRecentAnimeRecommendations(data);
   return (
     <div className="recent-recommendations">
       <div className="title_fz25fw500">Recent Recommendations</div>
       <div className="recent-recommendations__list">
-        <div className="recent-recommendations__card">
-          <img src="https://m.media-amazon.com/images/M/MV5BM2YwYTkwNjItNGQzNy00MWE1LWE1M2ItOTMzOGI1OWQyYjA0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UX1000_.jpg" alt="" className="recent-recommendations__card-img" />
-          <div className="recent-recommendations__card-container">
-            <div className="title_fz16fw500">Arifureta: From Commonplace to World's Strongest Season 3</div>
-            <div className="recent-recommendations__card-info">
-              <div>TV</div>
-              <div className="recent-recommendations__card-info-divider">---</div>
-              <div>Currently Airing</div>
-              <div className="recent-recommendations__card-info-divider">---</div>
-              <div>12 Episodes</div>
-            </div>
-          </div>
-        </div>
+        {items}
       </div>
     </div>
   )
