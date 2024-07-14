b3f1f9b65e7bd903ec88af112599711670bf7d8b
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-07-14 18:28:39 +0300

    add hamburger for screen widths below 768px

--- src/components/appHeader/AppHeader.jsx
+++ src/components/appHeader/AppHeader.jsx
@@ -1,10 +1,27 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 import AnimeSearchForm from "../animeSearchForm/AnimeSearchForm";
 
 import './appHeader.scss';
 
 const AppHeader = () => {
+  const location = useLocation();
+  const isSearchPage = location.pathname.startsWith('/search');
+
+  const [isMobileScreen, setIsMobileScreen] = useState(window.innerWidth < 768);
+  const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+
+  useEffect(() => {
+    setIsMobileScreen(window.innerWidth < 768);
+  }, []); 
+
+  const onHamburgerClick = () => {
+    setIsHamburgerActive(!isHamburgerActive);
+  }
+
+  const hamburgerClassName = `hamburger ${isHamburgerActive ? 'hamburger_active' : ''}`;
+
   return (
     <header className="app__header">
       <Link to='/' className="app__header-logo">Anime Surf</Link>
@@ -12,6 +29,13 @@ const AppHeader = () => {
       <button className="button">
         <Link to='/login'>Sign in</Link>
       </button>
+      {isSearchPage && isMobileScreen && (
+        <div className={hamburgerClassName} onClick={onHamburgerClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      )}
     </header>
   )
 } 
