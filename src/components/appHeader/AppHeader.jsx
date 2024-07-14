4b60dd9f5fd8cdebe6202779323fc9786ac39cbf
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-07-14 19:23:23 +0300

    add search button in AppHeader instead of input for screen widths below 768px

--- src/components/appHeader/AppHeader.jsx
+++ src/components/appHeader/AppHeader.jsx
@@ -11,31 +11,52 @@ const AppHeader = () => {
 
   const [isMobileScreen, setIsMobileScreen] = useState(window.innerWidth < 768);
   const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+  const [isSearchFormVisible, setIsSearchFormVisible] = useState(false);
 
   useEffect(() => {
-    setIsMobileScreen(window.innerWidth < 768);
-  }, []); 
+    const handleResize = () => {
+      setIsMobileScreen(window.innerWidth < 768);
+    };
+    
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [window.innerWidth]); 
 
   const onHamburgerClick = () => {
     setIsHamburgerActive(!isHamburgerActive);
   }
+
+  const onSearchButtonClick = () => {
+    setIsSearchFormVisible(!isSearchFormVisible);
+  };
 
   const hamburgerClassName = `hamburger ${isHamburgerActive ? 'hamburger_active' : ''}`;
 
   return (
     <header className="app__header">
       <Link to='/' className="app__header-logo">Anime Surf</Link>
-      <AnimeSearchForm />
-      <button className="button">
-        <Link to='/login'>Sign in</Link>
-      </button>
-      {isSearchPage && isMobileScreen && (
-        <div className={hamburgerClassName} onClick={onHamburgerClick}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      )}
+      {!isMobileScreen && <AnimeSearchForm />}
+      <div className="app__header-wrapper">
+        {isMobileScreen && (
+          <button className="button button__search" onClick={onSearchButtonClick}>
+            <i className="icon-search"></i>
+          </button>
+        )}
+        <button className="button">
+          <Link to='/login'>Sign in</Link>
+        </button>
+        {isSearchPage && isMobileScreen && (
+          <div className={hamburgerClassName} onClick={onHamburgerClick}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        )}
+      </div>
     </header>
   )
 } 
