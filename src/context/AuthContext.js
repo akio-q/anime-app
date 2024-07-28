255f09d6d69bd7e8b5a7fbf8f1ddaecebcd97c24
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-07-28 18:50:38 +0300

    add AuthContext to provide currentUser

---
+++ src/context/AuthContext.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { auth } from '../config/firebase'
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect } from "react";
+
+export const AuthContext = createContext(); 
+
+export const AuthContextProvider = ({children}) => {
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, user => { 
+      setCurrentUser(user); 
+      console.log(user);
+    });
+
+    return () => {
+      unsub();
+    } 
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{currentUser}}>
+      {children}
+    </AuthContext.Provider>
+  )  
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { AuthContextProvider } from './context/AuthContext';
 
 import App from './components/app/App';
 import store from './store';
@@ -9,8 +10,10 @@ import './style/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <AuthContextProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthContextProvider>
 );
 
