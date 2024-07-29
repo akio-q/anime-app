8cefbf34aefcf9b3b7d269cf09ce1ea0edc2e80f
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-07-29 17:55:18 +0300

    add lazy pages loading

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,15 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 import AppHeader from '../appHeader/AppHeader';
-import Home from '../pages/Home';
-import SingleAnime from '../pages/singleAnimeLayout/SingleAnime';
-import SearchResults from '../pages/searchResultsLayout/SearchResults';
-import Register from "../pages/Register";
-import Login from "../pages/Login";
 import AppFooter from '../appFooter/AppFooter';
+import Spinner from '../Spinner/Spinner';
+
+const Home = lazy(() => import('../pages/Home'));
+const SingleAnime = lazy(() => import('../pages/singleAnimeLayout/SingleAnime'));
+const SearchResults = lazy(() => import('../pages/searchResultsLayout/SearchResults'));
+const Register = lazy(() => import("../pages/Register"));
+const Login = lazy(() => import("../pages/Login")); 
 
 const App = () => {
   return (
@@ -29,13 +32,15 @@ const Inner = () => {
     <>
       {!isLoginOrRegister() && <AppHeader />}
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/anime/:animeId' element={<SingleAnime />} />
-          <Route path='/search/:animeName' element={<SearchResults />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/anime/:animeId' element={<SingleAnime />} />
+            <Route path='/search/:animeName' element={<SearchResults />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Suspense>
       </main>
       {!isLoginOrRegister() && <AppFooter />}
     </>
