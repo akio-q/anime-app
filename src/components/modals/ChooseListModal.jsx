19148825b3f33fb5893e7332349103b79e0cdfd2
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-08-04 14:05:50 +0300

    add ChooseListModal component

---
+++ src/components/modals/ChooseListModal.jsx
@@ -0,0 +1,36 @@
+import { IoMdClose } from "react-icons/io";
+import '../../style/modal.scss';
+
+const ChooseListModal = ({ isOpen, onClose, onChoose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <IoMdClose 
+          className="modal__close"
+          onClick={onClose} />
+        <div className='title_fz25fw600 modal__title'>Choose List</div>
+        <ul className="modal__list">
+          <li 
+            className='modal__list-item' 
+            onClick={() => onChoose('watching')}>Watching</li>
+          <li 
+            className='modal__list-item' 
+            onClick={() => onChoose('completed')}>Completed</li>
+          <li 
+            className='modal__list-item' 
+            onClick={() => onChoose('planned')}>Planned</li>
+          <li 
+            className='modal__list-item' 
+            onClick={() => onChoose('on-hold')}>On Hold</li>
+          <li 
+            className='modal__list-item' 
+            onClick={() => onChoose('dropped')}>Dropped</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ChooseListModal;
