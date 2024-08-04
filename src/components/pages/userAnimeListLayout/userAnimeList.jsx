1f65986ea7f6f6b9af05ab595bd0391c7c412ca8
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-08-04 20:00:15 +0300

    display saved anime in tabs on userAnimeList page

--- src/components/pages/userAnimeListLayout/userAnimeList.jsx
+++ src/components/pages/userAnimeListLayout/userAnimeList.jsx
@@ -1,17 +1,46 @@
-import React, { useState, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { AuthContext } from "../../../context/AuthContext";
+import { getDocs, collection, query, getDoc, doc } from 'firebase/firestore';
+import { db } from '../../../config/firebase';
+
+import AnimeCard from '../../animeCard/AnimeCard';
+import Spinner from '../../Spinner/Spinner';
+import ErrorMessage from '../../errorMessage/ErrorMessage';
 
 import './userAnimeList.scss';
 
 const UserAnimeList = () => {
-  const [activeTab, setActiveTab] = useState('tab1');
+  const [activeTab, setActiveTab] = useState('watching');
+  const [animeList, setAnimeList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const { currentUser } = useContext(AuthContext);
   const joinDate = currentUser?.metadata?.creationTime;
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
+  useEffect(() => {
+    const fetchAnimeList = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const animeListRef = doc(db, 'users', currentUser.uid, 'animeLists', activeTab);
+        const querySnapshot = await getDoc(animeListRef);
+
+        if (querySnapshot.exists()) {
+          setAnimeList(querySnapshot.data().anime);
+        } else {
+          setAnimeList([]);
+        }
+      } catch (err) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAnimeList();
+  }, [activeTab, currentUser.uid]);
 
   return (
     <div className='user-anime-list'>
@@ -24,42 +53,51 @@ const UserAnimeList = () => {
         <div className="title_fz30fw600">{`${currentUser.displayName}'s Anime List`}</div>
         <div className="user-anime-list__tabs">
           <button 
-            className={`user-anime-list__tab ${activeTab === 'tab1' ? 'active' : ''}`} 
-            onClick={() => handleTabClick('tab1')}
-          >
+            className={`user-anime-list__tab ${activeTab === 'watching' ? 'active' : ''}`} 
+            onClick={() => setActiveTab('watching')}>
             Watching
           </button>
           <button 
-            className={`user-anime-list__tab ${activeTab === 'tab2' ? 'active' : ''}`} 
-            onClick={() => handleTabClick('tab2')}
-          >
+            className={`user-anime-list__tab ${activeTab === 'completed' ? 'active' : ''}`} 
+            onClick={() => setActiveTab('completed')}>
             Completed
           </button>
           <button 
-            className={`user-anime-list__tab ${activeTab === 'tab3' ? 'active' : ''}`} 
-            onClick={() => handleTabClick('tab3')}
-          >
+            className={`user-anime-list__tab ${activeTab === 'planned' ? 'active' : ''}`} 
+            onClick={() => setActiveTab('planned')}>
             Planned
           </button>
           <button 
-            className={`user-anime-list__tab ${activeTab === 'tab3' ? 'active' : ''}`} 
-            onClick={() => handleTabClick('tab3')}
-          >
+            className={`user-anime-list__tab ${activeTab === 'on-hold' ? 'active' : ''}`} 
+            onClick={() => setActiveTab('on-hold')}>
             On Hold
           </button>
           <button 
-            className={`user-anime-list__tab ${activeTab === 'tab3' ? 'active' : ''}`} 
-            onClick={() => handleTabClick('tab3')}
-          >
+            className={`user-anime-list__tab ${activeTab === 'dropped' ? 'active' : ''}`} 
+            onClick={() => setActiveTab('dropped')}>
             Dropped
           </button>
         </div>
-        <div className="user-anime-list__items">
-          {activeTab === 'tab1' && <div>Content for Tab 1</div>}
-          {activeTab === 'tab2' && <div>Content for Tab 2</div>}
-          {activeTab === 'tab3' && <div>Content for Tab 3</div>}
-          {activeTab === 'tab4' && <div>Content for Tab 3</div>}
-          {activeTab === 'tab5' && <div>Content for Tab 3</div>}
+        <div className="user-anime-list__container">
+        {loading ? (
+            <div className="user-anime-list__wrapper">
+              <Spinner />
+            </div>
+          ) : error ? (
+            <div className="user-anime-list__wrapper">
+              <ErrorMessage />
+            </div>
+          ) : animeList.length === 0 ? (
+            <div className="user-anime-list__wrapper title_fz16fw300">
+              No anime found in this list.
+            </div>
+          ) : (
+            <div className="anime__list">
+              {animeList.map((anime) => (
+                <AnimeCard key={anime.animeId} data={anime.data} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
