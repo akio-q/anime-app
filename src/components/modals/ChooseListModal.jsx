d85b5a85dcaaa4f65aeddf25ee6c92efdd3be6a4
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-08-05 12:43:38 +0300

    close ChooseListModal on overlay click

--- src/components/modals/ChooseListModal.jsx
+++ src/components/modals/ChooseListModal.jsx
@@ -5,11 +5,9 @@ const ChooseListModal = ({ isOpen, onClose, onChoose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay">
+    <div className="modal-overlay" onClick={onClose}>
       <div className="modal">
-        <IoMdClose 
-          className="modal__close"
-          onClick={onClose} />
+        <IoMdClose className="modal__close" onClick={onClose} />
         <div className='title_fz25fw600 modal__title'>Choose List</div>
         <ul className="modal__list">
           <li 
