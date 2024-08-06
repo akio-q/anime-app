6b6d41a243b505a6f5d10eafa56815396bc36a5e
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-08-06 17:17:30 +0300

    add Pagination component

---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,40 @@
+import { GrPrevious } from "react-icons/gr";
+import { GrNext } from "react-icons/gr";
+
+import './pagination.scss'; 
+
+const Pagination = ({ currentPage, totalPages, handlePageChange }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className="pagination">
+      <button
+        onClick={() => handlePageChange(currentPage - 1)} 
+        disabled={currentPage === 1} 
+        className="pagination__navigation">
+          <GrPrevious />
+      </button>
+      {pageNumbers.map(number => (
+        <ul className="pagination__list">
+          <li
+            key={number}
+            onClick={() => handlePageChange(number)}
+            className={`pagination__item ${number === currentPage ? 'active' : ''}`}>
+            {number}
+          </li>
+        </ul>
+      ))}
+      <button 
+        onClick={() => handlePageChange(currentPage + 1)} 
+        disabled={currentPage === totalPages} 
+        className="pagination__navigation">
+          <GrNext />
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
