6ac1a2e4250474cc0a8260a34ed02eb343b14471
Valentyn Vovk <wolf.valentyn@gmail.com>
2024-08-11 13:11:49 +0300

    add link to my portfolio website in AppFooter

--- src/components/appFooter/AppFooter.jsx
+++ src/components/appFooter/AppFooter.jsx
@@ -4,7 +4,10 @@ const AppFooter = () => {
   const date = new Date().getFullYear()>2024&&document.write("-"+new Date().getFullYear());
   return (
     <footer className="app__footer">
-        <a href="#" className="app__footer-link">2024{date} AniSurf, © All rights reserved </a>
+      <a href="https://valentynvowk.netlify.app/" className="app__footer-link" target="_blank" rel="noopener noreferrer">
+        Check out my portfolio
+      </a>
+      <a href="#" className="app__footer-copy">2024{date} AniSurf, © All rights reserved </a>
     </footer>
   )
 }
